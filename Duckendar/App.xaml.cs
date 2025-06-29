9808878d78917b0ea7cd65345b9d5aacfe11c80d
x99oly <oliveira.samuel.edu@gmail.com>
2025-06-29 16:46:13 -0300

    Adicionar arquivos de projeto.

---
+++ Duckendar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Duckendar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
